67a5c72060ba0d3f847171563b1d5062eb6d2ecf
roff-dev <kieronoates@hotmail.co.uk>
2024-12-08 16:33:06 +0000

    php integration
    index uses include of nav

--- index.php
+++ index.php
@@ -1,54 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="img/pixelroff.png" type="image/png">
-    <link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" href="img/icons/style.css">
-    <title>K O Portfolio</title>
-</head>
-<body id="nav-top">
-    <!-- 
-    ///////////////////////////////////// NAV ////////////////////////////////////////////
-    -->
-    <div class="navbar" id="navbar back-to-top">
-        <div class="nav-logo">
-            <ul>
-                <li><a href="index.html"><img class="logo-image" src="img/pixelroff.png" alt=""></a></li>
-            </ul>
-        </div>
-        <div class="hamburger" id="hamburger">
-            <a href="#nav-top"><span>&#9776;</span></a>
-        </div>
-        <div class="nav-links" id="nav-links">
-            <ul>
-                <li><a href="about.html">About Me</a></li>
-                <li><a href="#portfolio">Projects</a></li>
-                <li><a href="code-examples.html">Code</a></li>
-                <li class="scs-scheme"><a href="scs.html">SCS Scheme</a></li>
-                <li class="contact-me"><a href="#get-in-touch">Contact</a></li>
-            </ul>
-        </div>
-        <div class="socials" id="socials">
-            <ul>
-                <li id="linkedin"><a href="https://www.linkedin.com/in/kieron-oates-b061b3317/" target="_blank"><span class="icon-linkedin"></span></a></li>
-                <li id="github"  ><a href="https://github.com/roff-dev" target="_blank"><span class="icon-github"></span></a></li>
-            </ul>
-        </div>
-    </div>
-    <!-- 
-    ////////////////////////////////////////////////////////////////////////////////////////
-    -->
+<?php
 
-    <!-- 
-    ///////////////////////////////////// BACKGROUND ////////////////////////////////////////////
-    -->
-    <canvas id="backgroundCanvas"></canvas>
-    <!-- 
-    //////////////////////////////////////////////////////////////////////////////////////
-    -->
+include ("inc/nav.php");
 
+?>
+    <!-- <body> -->
     <!-- 
     ///////////////////////////////////// HERO CONSOLE ////////////////////////////////////////////
     -->
