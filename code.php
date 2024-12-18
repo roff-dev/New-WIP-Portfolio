51381cdff2edc918f77dafda138a84ce1cb01edc
roff-dev <kieronoates@hotmail.co.uk>
2024-12-18 18:33:29 +0000

    code snippet start

--- code.php
+++ code.php
@@ -1,7 +1,30 @@
 <?php include ("inc/nav.php"); ?>
 
+<!-- MAIN CONTAINER -->
 <div class="container">
-    <h1>this page clearly isnt finished LEAVE NOW</h1>
+    <!-- code snippet container -->
+    <div class="snippets-list">
+        <div class="code-snippet">
+            <div class="snippet-title"><h3>Project 1 - tester tester</h3></div>
+            <div class="snippet-dropdown"><a href=""><span class="icon-keyboard_arrow_down"></span></a></div>
+        </div>
+
+        <div class="code-snippet">
+            <div class="snippet-title"><h3>Project 1 - tester tester</h3></div>
+            <div class="snippet-dropdown"><span class="icon-keyboard_arrow_down"></span></div>
+        </div>
+
+        <div class="code-snippet">
+            <div class="snippet-title"><h3>Project 1 - tester tester</h3></div>
+            <div class="snippet-dropdown"><span class="icon-keyboard_arrow_down"></span></div>
+        </div>
+
+        <div class="code-snippet">
+            <div class="snippet-title"><h3>Project 1 - tester tester</h3></div>
+            <div class="snippet-dropdown"><span class="icon-keyboard_arrow_down"></span></div>
+        </div>
+    </div>
+
 </div>
 
 <?php include ("inc/footer.php"); ?>
