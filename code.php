a1a741ffaa509fcff11940b309c8ecb2f257e36a
roff-dev <kieronoates@hotmail.co.uk>
2024-12-19 10:15:17 +0000

    code block added

--- code.php
+++ code.php
@@ -5,25 +5,110 @@
     <!-- code snippet container -->
     <div class="snippets-list">
         <div class="code-snippet">
-            <div class="snippet-title"><h3>Project 1 - tester tester</h3></div>
-            <div class="snippet-dropdown"><a href=""><span class="icon-keyboard_arrow_down"></span></a></div>
-            <div class="code-window"></div>
+            <div class="snippet-header">
+                <div class="snippet-title"><h3>#1 - User Authentication</h3></div>
+                <div class="snippet-dropdown"><a href=""><span class="icon-keyboard_arrow_down"></span></a></div>
+            </div>
+            <pre class="code-block">
+                <code class="language-javascript">//Using web tokens to authenticate users
+const jwt = require("jsonwebtoken");
+const jwtSecret =
+  "4715aed3c946f7b0a38e6b534a9583628d84e96d10fbc04700770d572af3dce43625dd";
+
+exports.adminAuth = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, jwtSecret, (err, decodedToken) => {
+      if (err) {
+        return res.status(401).json({ message: "Not authorized" });
+      } else {
+        if (decodedToken.role !== "admin") {
+          return res.status(401).json({ message: "Not authorized" });
+        } else {
+          next();
+        }
+      }
+    });
+  } else {
+    return res
+      .status(401)
+      .json({ message: "Not authorized, token not available" });
+  }
+};
+exports.userAuth = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, jwtSecret, (err, decodedToken) => {
+      if (err) {
+        return res.status(401).json({ message: "Not authorized" });
+      } else {
+        if (decodedToken.role !== "Basic") {
+          return res.status(401).json({ message: "Not authorized" });
+        } else {
+          next();
+        }
+      }
+    });
+  } else {
+    return res
+      .status(401)
+      .json({ message: "Not authorized, token not available" });
+  }
+};</code></pre>
+        </div>
+        <div class="code-snippet">
+            <div class="snippet-header">
+                <div class="snippet-title"><h3>#1 - User Authentication</h3></div>
+                <div class="snippet-dropdown"><a href=""><span class="icon-keyboard_arrow_down"></span></a></div>
+            </div>
+            <pre class="code-block">
+                <code class="language-javascript">//Using web tokens to authenticate users
+const jwt = require("jsonwebtoken");
+const jwtSecret =
+  "4715aed3c946f7b0a38e6b534a9583628d84e96d10fbc04700770d572af3dce43625dd";
+
+exports.adminAuth = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, jwtSecret, (err, decodedToken) => {
+      if (err) {
+        return res.status(401).json({ message: "Not authorized" });
+      } else {
+        if (decodedToken.role !== "admin") {
+          return res.status(401).json({ message: "Not authorized" });
+        } else {
+          next();
+        }
+      }
+    });
+  } else {
+    return res
+      .status(401)
+      .json({ message: "Not authorized, token not available" });
+  }
+};
+exports.userAuth = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, jwtSecret, (err, decodedToken) => {
+      if (err) {
+        return res.status(401).json({ message: "Not authorized" });
+      } else {
+        if (decodedToken.role !== "Basic") {
+          return res.status(401).json({ message: "Not authorized" });
+        } else {
+          next();
+        }
+      }
+    });
+  } else {
+    return res
+      .status(401)
+      .json({ message: "Not authorized, token not available" });
+  }
+};</code></pre>
         </div>
 
-        <div class="code-snippet">
-            <div class="snippet-title"><h3>Project 1 - tester tester</h3></div>
-            <div class="snippet-dropdown"><span class="icon-keyboard_arrow_down"></span></div>
-        </div>
-
-        <div class="code-snippet">
-            <div class="snippet-title"><h3>Project 1 - tester tester</h3></div>
-            <div class="snippet-dropdown"><span class="icon-keyboard_arrow_down"></span></div>
-        </div>
-
-        <div class="code-snippet">
-            <div class="snippet-title"><h3>Project 1 - tester tester</h3></div>
-            <div class="snippet-dropdown"><span class="icon-keyboard_arrow_down"></span></div>
-        </div>
     </div>
 
 </div>
