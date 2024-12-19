6e307c2199b7d366dbb6bdf87c261906fa6c374f
roff-dev <kieronoates@hotmail.co.uk>
2024-12-19 23:20:05 +0000

    scs content

--- scs.php
+++ scs.php
@@ -2,9 +2,29 @@
 
 <div class="container">
 <div id="scs-fullpage">
-    <div class="scs-section">First section</div>
-    <div class="scs-section">Second section</div>
-    <div class="scs-section">Third section</div>
+    <div class="scs-section">
+        <h1>Scion Coalition Scheme</h1>
+        <p>The Scion Coalition Scheme is an intensive, specially tailored training program run by Netmatters in order to give willing candidates the opportunity to enter the industry as web developers. Under the supervision of senior web developers, scions generally aim to complete training within six to nine months. The course is intensive and therefore the level of learning achieved is extensive in a short space of time.</p>
+    </div>
+    <div class="scs-section">
+        <h1>Treehouse</h1>
+        <p>Treehouse is an online learning community, featuring videos covering a number of topics from basic HTML to C# programming, iOS development, data analysis, and more. By completing courses users can earn points, allowing them to track their progress and see how much they've covered in certain areas.</p>
+        <span>Treehouse Score:</span>
+        <span>View Treehouse Profile</span>
+    </div>
+    <div class="scs-section">
+        <h1>About Netmatters</h1>
+        <ul class="net-list">
+            <li>Established in 2008</li>
+            <li>Norfolk's leading technology company</li>
+            <li>Winner of the Princess Royal Training Award</li>
+            <li>Winner of EDP Skills of Tomorrow Award</li>
+            <li>80+ staff, 2 locations across Norfolk</li>
+            <li>Digital Marketing, Website & Software development & IT Support</li>
+            <li>Broad spectrum of clients, working nationwide</li>
+            <li>Operate to strict company values</li>
+        </ul>
+    </div>
 </div>
 </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/4.0.20/fullpage.min.js"></script>
