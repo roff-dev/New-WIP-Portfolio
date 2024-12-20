9a115bda8d22b55242e25704d00078d91922cbde
roff-dev <kieronoates@hotmail.co.uk>
2024-12-20 14:32:54 +0000

    more strict email regex

--- inc/process_contact.php
+++ inc/process_contact.php
@@ -24,7 +24,7 @@ try {
     }
 
     // Validate email format
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)|| !preg_match('/^(([^<>()[]\.,;:\s@"]+(.[^<>()[]\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/', $email)) {
         throw new Exception('Invalid email format');
     }
     
