fa15c66978310c61a40dcbb8f510ce95d8f08a11
roff-dev <kieronoates@hotmail.co.uk>
2025-02-22 16:08:23 +0000

    project image and pagination

--- code.php
+++ code.php
@@ -18,13 +18,59 @@ CODE SNIPPET FORMAT
 <div class="container">
     <!-- code snippet container -->
     <div class="snippets-list">
-      <?php
-        include 'inc/connection.php'; 
+    <?php
+        include 'inc/connection.php';
 
-        // Fetch all snippets
-        $query = $pdo->query("SELECT * FROM snippets ORDER BY id DESC");
+        // Number of records per page
+        $records_per_page = 5;
+
+        // Get the current page number from URL, default to 1 if not set
+        $current_page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+
+        // Calculate the offset for the SQL query
+        $offset = ($current_page - 1) * $records_per_page;
+
+        // Get total number of records for pagination
+        $total_query = $pdo->query("SELECT COUNT(*) FROM snippets");
+        $total_records = $total_query->fetchColumn();
+
+        // Calculate total number of pages
+        $total_pages = ceil($total_records / $records_per_page);
+
+        // Fetch snippets for current page
+        $query = $pdo->prepare("SELECT * FROM snippets ORDER BY id DESC LIMIT :limit OFFSET :offset");
+        $query->bindValue(':limit', $records_per_page, PDO::PARAM_INT);
+        $query->bindValue(':offset', $offset, PDO::PARAM_INT);
+        $query->execute();
         $snippets = $query->fetchAll(PDO::FETCH_ASSOC);
-      ?>
+
+        // Generate pagination links
+        $pagination = '';
+        if ($total_pages > 1) {
+            $pagination .= '<div class="pagination">';
+            
+            // Previous page link
+            if ($current_page > 1) {
+                $pagination .= '<a href="?page='.($current_page - 1).'">&laquo; Previous</a> ';
+            }
+            
+            // Page numbers
+            for ($i = 1; $i <= $total_pages; $i++) {
+                if ($i == $current_page) {
+                    $pagination .= '<span class="current">'.$i.'</span> ';
+                } else {
+                    $pagination .= '<a href="?page='.$i.'">'.$i.'</a> ';
+                }
+            }
+            
+            // Next page link
+            if ($current_page < $total_pages) {
+                $pagination .= '<a href="?page='.($current_page + 1).'">Next &raquo;</a>';
+            }
+            
+            $pagination .= '</div>';
+        }
+        ?>
         <div class="snippet-search">
             <div class="search-wrapper">
                 <svg class="search-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
@@ -56,10 +102,6 @@ CODE SNIPPET FORMAT
                         <input type="checkbox" value="php">
                         <span class="language-tag php">PHP</span>
                     </label>
-                    <label class="tag-option">
-                        <input type="checkbox" value="html">
-                        <span class="language-tag html">HTML</span>
-                    </label>
                 </div>
             </div>
         </div>
@@ -68,13 +110,13 @@ CODE SNIPPET FORMAT
               <div class="snippet-header">
                   <div class="snippet-title">
                         <div class="snippet-left">
-                            <a href="index.php#portfolio"><img src="img/disspic.png" alt="project image"></a>
+                            <a href="index.php#portfolio"><img src="<?= htmlspecialchars($snippet['source']); ?>" alt="project image"></a>
                             <h3><?= htmlspecialchars($snippet['title']); ?></h3>
                         </div>
                         <div class="snippet-tags">
-                        <span class="language-tag <?= htmlspecialchars($snippet['language']); ?>">
-    <?= strtoupper(htmlspecialchars($snippet['language'])); ?>
-</span>
+                            <span class="language-tag <?= htmlspecialchars($snippet['language']); ?>">
+                                <?= strtoupper(htmlspecialchars($snippet['language'])); ?>
+                            </span>
                         </div>
                   </div>
                   <div class="snippet-dropdown">
@@ -88,6 +130,7 @@ CODE SNIPPET FORMAT
               </pre>
           </div>
         <?php endforeach; ?>
+        <?php echo $pagination; ?>
     </div>
 </div>
 <script src="js/prism.js"></script>
